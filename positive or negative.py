e950d175f6027a0f6e7323728471db1bc7cc7eb2
AbiNeya <46442802+AbiNeya@users.noreply.github.com>
2019-01-25 14:35:46 +0530

    Update positive or negative.py

--- positive or negative.py
+++ positive or negative.py
@@ -1,7 +1,7 @@
 a=int(input("Enter the Number:"))
-if(a>0):
-  print("positive")
- elif(a==o):
-  print("zero")
- else:
+if(a<0):
   print("Negative")
+else if(a>0):
+  print("Positive")
+else:
+  print("Zero")
