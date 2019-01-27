254c54411e6b479f9221b680698479e35f40b534
AbiNeya <46442802+AbiNeya@users.noreply.github.com>
2019-01-26 23:10:22 -1200

    Update vowel or consonant.py

--- vowel or consonant.py
+++ vowel or consonant.py
@@ -1,7 +1,6 @@
-
-ch=input("Enter a character:")
+ ch=input("Enter a character:")
 if(ch=='A'or ch=='a'or ch=='E'or ch=='e'or ch=='I'or ch=='i' or ch=='O'or ch=='o'or ch=='u'or ch=='U'):
-    print("The letter u have entered is a VOWEL")
+    print(" VOWEL")
 else:
-    print("The letter u have entered is a CONSONANT")
+    print(" CONSONANT")
     
